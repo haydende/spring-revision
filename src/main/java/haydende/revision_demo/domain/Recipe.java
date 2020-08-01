17000c06a154b2311d466a8dacf2f16a388b9480
haydende <haydeneastwell@outlook.com>
2020-08-01 20:27:14 +0100

    Added Recipe class (with Getters and Setters courtesy of Project Lombok).
    Closes #1

---
+++ src/main/java/haydende/revision_demo/domain/Recipe.java
@@ -0,0 +1,28 @@
+package haydende.revision_demo.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Recipe extends BaseEntity {
+
+    private final String name;
+    private final String description;
+    private final String directions;
+    private final int cookTime;
+    private final int prepTime;
+    private final int servings;
+
+    @Builder
+    public Recipe(Long id, String name, String description, String directions, int cookTime, int prepTime, int servings) {
+        super(id);
+        this.name = name;
+        this.description = description;
+        this.directions = directions;
+        this.cookTime = cookTime;
+        this.prepTime = prepTime;
+        this.servings = servings;
+    }
+}
