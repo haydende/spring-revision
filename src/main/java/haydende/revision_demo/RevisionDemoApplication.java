3d74b591fbc01f5f9f962d804c952c80f77b77e4
haydende <haydeneastwell@outlook.com>
2020-08-01 17:56:07 +0100

    Initial project added

---
+++ src/main/java/haydende/revision_demo/RevisionDemoApplication.java
@@ -0,0 +1,13 @@
+package haydende.revision_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevisionDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/haydende/revision_demo/RevisionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package haydende.revision_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisionDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
